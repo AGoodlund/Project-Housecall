dea2483006382e1fb114708c6f5c65a1e1ecc2d1
AshxntiM21 <144647048+AshxntiM21@users.noreply.github.com>
2025-10-18 09:48:24 -0700

    Add files via upload

--- requestagent.py
+++ requestagent.py
@@ -1,18 +1,23 @@
 
 import requests
 def get_data_from_website(search_query):
-    base_url = "https://mapper.dpcfrontier.com"
+    base_url = ""
     params = {"q": search_query}
     try:
-        response = requests.get(base_url, params=params)
+        response = response.get(base_url,params=params)
         response.raise_for_status() #raise exception for http errors
-        return response.text #or response.json() if its an API
+        directProvider= response.text #or response.json() if its an API
+        content_div = directProvider.find('div',id="ProviderData")
+        if content_div:
+            return content_div.get_text(separator = "\n", stip=True)
+        else:
+            return
     except requests.exceptions.RequestException as e:
         print(f"error fetching data: {e}")
         return None
     
 #example usage
-second_input = "95747"
+second_input = "cloud computing benefits"
 website_data = get_data_from_website(second_input)
 if website_data:
     print(f"data from website: {website_data[:200]}...")
